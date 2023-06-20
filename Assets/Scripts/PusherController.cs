8bb8d09352898eed4c2851c369eb440f7ef520b0
Tim Keicher <keicher04@gmail.com>
2023-06-20 10:24:52 +0200

    eliminate offset in PusherController for pusherHuman

--- Assets/Scripts/PusherController.cs
+++ Assets/Scripts/PusherController.cs
@@ -122,7 +122,7 @@ public class PusherController : MonoBehaviour
                 {
                     // set target position
                     pusherActuatorX.Control = -targetPosition.x;
-                    pusherActuatorZ.Control = -targetPosition.y;
+                    pusherActuatorZ.Control = -targetPosition.y - 42; // offset between AirHockeyTable and Pusher coordinate system
                 }
                 else
                 {
